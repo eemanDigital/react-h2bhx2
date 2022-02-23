3f0d5b4a482bc2c8a4c22b53c8ffe2721cb01550
eemanDigital <eemandigitalconcept@gmail.com>
2022-02-23 09:37:50 +0100

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,65 +1,65 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 import Todo from './components/Todo';
 import Form from './components/Form';
 import FilterButton from './components/FilterButton';
+import { nanoid } from 'nanoid';
 
 function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
 
-    const [tasks, setTasks] = useState(props.tasks)
-    
-   
+  function toggleCompleted(id) {
+    function toggleTaskCompleted(id) {
+      const updatedTasks = tasks.map((task) => {
+        // if this task has the same ID as the edited task
+        if (id === task.id) {
+          // use object spread to make a new object
+          // whose `completed` prop has been inverted
+          return { ...task, completed: !task.completed };
+        }
+        return task;
+      });
+      setTasks(updatedTasks);
 
-  const taskLists =tasks.map((task) => (
+    }
+
+    console.log(tasks[0]);
+  }
+  const taskLists = tasks.map((task) => (
     <Todo
       id={task.id}
       name={task.name}
       completed={task.completed}
       key={task.id}
+      toggleCompleted={toggleCompleted}
     />
   ));
   function addTask(name) {
-    const newTask = { id: "id", name: name, completed: false };
+    const newTask = { id: 'todo' + nanoid(), name: name, completed: false };
     setTasks([...tasks, newTask]);
   }
+
+  const tasksNoun = taskLists.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskLists.length} ${tasksNoun} remaining`;
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
 
-      <Form addTask= {addTask}/>
+      <Form addTask={addTask} />
 
       <div className="filters btn-group stack-exception">
-        {/* <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button> */}
-
-        <FilterButton position = "All" aria_pressed ={true} />
-        <FilterButton position="Active"  aria_pressed ={false}/>
-        <FilterButton position="Completed" aria_pressed ={false}/>
+        <FilterButton position="All" aria_pressed={true} />
+        <FilterButton position="Active" aria_pressed={false} />
+        <FilterButton position="Completed" aria_pressed={false} />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-        {/* <Todo name = "Eat" completed= {true} id= "todp-0"/>
-        <Todo name = "Sleep" completed= {false} id= "todp-1"/>
-        <Todo name = "Repeat" completed= {false}  id= "todp-2"/> */}
         {taskLists}
       </ul>
     </div>
